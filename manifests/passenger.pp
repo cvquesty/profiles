028d73e3d224ec548f2e914995b3d2d2f8f7c8d2
root <root@master.puppetlabs.vm>
2016-08-04 15:50:30 +0000

    adding more dependencies.

--- manifests/passenger.pp
+++ manifests/passenger.pp
@@ -1,10 +1,28 @@
 # Sample profile to load passenger
 #
 class profiles::passenger {
-  class { '::passenger': }
 
   package { 'rubygems':
     ensure => 'installed',
   }
 
+  package { 'ruby-devel':
+    ensure => 'installed',
+  }
+
+  package { 'gcc':
+    ensure => 'installed',
+  }
+
+  package { 'gcc-c++':
+    ensure => 'installed',
+  }
+
+  package { 'zlib-devel':
+    ensure => 'installed',
+  }
+
+  class { '::passenger':
+    require => Package['rubygems'],
+  }
 }
