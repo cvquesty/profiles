66b8ac116a68eac31a729b59adc8237b47926e15
Jerald Sheets <questy@gmail.com>
2016-08-08 15:04:02 -0400

    Adding the VirutalHost

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -6,4 +6,10 @@ class profiles::apache {
   class { '::epel': }
   class { '::apache::mod::passenger': }
 
+  apache::vhost { 'localhost':
+    priority => '10',
+    port     => '80',
+    docroot  => '/opt/forge/public',
+    logroot  => '/opt/forge/log',
+  }
 }
