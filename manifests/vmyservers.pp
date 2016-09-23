6681976c195ac7f85b2093dd878e4a6ed36eb23b
Jerald Sheets <jsheetsjr@paypal.com>
2016-09-23 12:30:55 -0700

    trying to revoke privileges on a user.

--- manifests/vmyservers.pp
+++ manifests/vmyservers.pp
@@ -13,7 +13,7 @@ class profiles::vmyservers {
     location => 'localhost',
   }
 
-  vmyusers::grant::admin { $name:
+  vmyusers::grant::noaccess { $name:
     dbauth   => 'root',
     dbauthpw => 'rootpw',
     user     => 'bob',
