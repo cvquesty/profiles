3d833265677c42d4aefae5a366800a4ab8637c49
Jerald Sheets <jsheetsjr@paypal.com>
2016-09-23 07:55:05 -0700

    adding profile call to vmyusers module to create mysql user.

--- manifests/vmyservers.pp
+++ manifests/vmyservers.pp
@@ -4,4 +4,11 @@ class profiles::vmyservers {
 
   include vmyusers::mysql
 
+  vmyusers::create::user { 'bob':
+    dbauth   => 'root',
+    dbauthpw => 'rootpw',
+    user     => 'bob',
+    password => 'bobpw',
+  }
+
 }
