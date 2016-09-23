ec015284f72e989a4186a87c670a1868df8035de
Jerald Sheets <jsheetsjr@paypal.com>
2016-09-23 09:41:21 -0700

    adding new parameter for manifest refactoring.

--- manifests/vmyservers.pp
+++ manifests/vmyservers.pp
@@ -5,11 +5,12 @@ class profiles::vmyservers {
   include vmyusers::mysql
 
   vmyusers::create::user { $name:
-    dbauth   => 'root',
-    dbauthpw => 'rootpw',
-    user     => 'bob',
-    database => 'bobdb',
-    location => 'localhost',
+    dbauth    => 'root',
+    dbauthpw  => 'rootpw',
+    user      => 'bob',
+    passsword => 'bobpw',
+    database  => 'bobdb',
+    location  => 'localhost',
   }
 
 }
