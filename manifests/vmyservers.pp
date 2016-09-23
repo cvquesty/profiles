55d4806ed80406d76840fe529132ad3f8f491aeb
Jerald Sheets <jsheetsjr@paypal.com>
2016-09-23 09:49:58 -0700

    Syntax error

--- manifests/vmyservers.pp
+++ manifests/vmyservers.pp
@@ -5,12 +5,12 @@ class profiles::vmyservers {
   include vmyusers::mysql
 
   vmyusers::create::user { $name:
-    dbauth    => 'root',
-    dbauthpw  => 'rootpw',
-    user      => 'bob',
-    passsword => 'bobpw',
-    database  => 'bobdb',
-    location  => 'localhost',
+    dbauth   => 'root',
+    dbauthpw => 'rootpw',
+    user     => 'bob',
+    password => 'bobpw',
+    database => 'bobdb',
+    location => 'localhost',
   }
 
 }
