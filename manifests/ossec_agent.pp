9a58cfdb4c8208349c12ca34e39ac28484c94a1e
cvquesty <questy@gmail.com>
2018-11-30 11:58:13 -0500

    Not sure if this will work, but worth a try. If I do not need to include the whole module for the profile, that makes it easy.

--- manifests/ossec_agent.pp
+++ manifests/ossec_agent.pp
@@ -1,8 +1,6 @@
 # Profile to configure an OSSEC HIDS Agent Node
 class profiles::ossec_agent {
 
-  include [ '::ossec' ]
-
   Class['::ossec::repo']
   -> Class['::ossec::agent']
 
--- manifests/ossec_server.pp
+++ manifests/ossec_server.pp
@@ -1,8 +1,6 @@
 # Profile to configure an OSSEC HIDS Server Node
 class profiles::ossec_server {
 
-  include [ '::ossec' ]
-
   Class['::ossec::repo']
   -> Class['::ossec::server']
 
