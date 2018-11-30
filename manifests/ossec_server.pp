e7212215508a939fe07a44509b49a8d355c8de1e
cvquesty <questy@gmail.com>
2018-11-30 15:52:56 -0500

    something is wrong with the commit. Manually rewrote it.

--- manifests/ossec_server.pp
+++ manifests/ossec_server.pp
@@ -1,7 +1,7 @@
 # Profile to configure an OSSEC HIDS Server Node
 class profiles::ossec_server {
 
-include ['::ossec::repo' ]
+include [ '::ossec::repo' ]
 include ['::ossec::server' ]
 
   Class['::ossec::repo']
