22e81677ab64c1edc961848658e6ce1925db3c8f
cvquesty <questy@gmail.com>
2018-12-06 10:33:50 -0500

    Changing the call in the profile to instantiate the class, and specify server function. This eliminates the duplicate resource problem, and makes the module extensible for as we add configs, they can be specific to server function rather than trying to do all sorts of gyrations to scoot by the error.

--- manifests/ossec_server.pp
+++ manifests/ossec_server.pp
@@ -1,10 +1,8 @@
 # Profile to configure an OSSEC HIDS Server Node
 class profiles::ossec_server {
 
-include [ '::ossec::repo' ]
-include ['::ossec::server' ]
-
-  Class['::ossec::repo']
-  -> Class['::ossec::server']
+  class { 'ossec':
+    nodetype => 'server',
+  }
 
 }
