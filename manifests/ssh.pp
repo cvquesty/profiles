7a4191e2a4a626f0998565605c367c462ed09843
cvquesty <questy@gmail.com>
2019-04-10 15:42:35 -0700

    configure SSH

---
+++ manifests/ssh.pp
@@ -0,0 +1,13 @@
+# Profile to configure SSH
+class profiles::ssh {
+
+  $sshport = lookup('profiles::ssh::sshport')
+  $options = lookup('profiles::ssh::options')
+
+  class { 'ssh':
+    storeconfigs_enabled => false,
+    server_options       => $options,
+    port                 => $sshport,
+  }
+
+}
