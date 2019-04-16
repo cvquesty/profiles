7037eda1a7ff03892495a6d3054d2e6674512cf3
cvquesty <questy@gmail.com>
2019-04-16 14:58:42 -0400

    setting up a rule

--- manifests/rsyslog.pp
+++ manifests/rsyslog.pp
@@ -1,6 +1,15 @@
 # Profile to configure Rsyslog
 class profiles::rsyslog {
 
+  # Configure the SysLog Daemon
   class { 'rsyslog': }
 
+  # Be sure to rotate the syslogs
+  logrotate::rule { 'messages':
+    path         => '/var/log/messages',
+    rotate       => '10',
+    rotate_every => 'day',
+    postrotate   => '/sbin/service rsyslog reload',
+    require      => Class['profiles::logrotate'],
+  }
 }
