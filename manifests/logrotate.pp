4770c9f478f14f8186bd76e3dd2a0b3f724986a6
cvquesty <questy@gmail.com>
2019-04-16 14:31:33 -0400

    adding in the logrotate profile

---
+++ manifests/logrotate.pp
@@ -0,0 +1,14 @@
+# This profile is for running logrotate
+class profiles::logrotate {
+
+  class { 'logrotate':
+    ensure => 'latest',
+    config => {
+      compress     => true,
+      rotate       => '10',
+      rotate_every => 'day',
+      ifempty      => true,
+    }
+  }
+
+}
