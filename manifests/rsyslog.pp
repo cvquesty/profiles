8c75feb93c8518d38dd90a25c6b3239d70cde9d3
Jerald Sheets <jsheets@ssconsultinggroup.net>
2022-08-31 10:54:48 -0400

    adding in rsyslog profile to manage logging

---
+++ manifests/rsyslog.pp
@@ -0,0 +1,14 @@
+# Profile to configure RSyslog
+class profiles::rsyslog {
+  # Configure the RSyslog Daemon
+  class { 'rsyslog': }
+
+  # Set logrotate rule for Syslog
+  logrotate::rule { 'messages':
+    path         => '/var/log/messages',
+    rotate       => 10,
+    rotate_every => 'day',
+    postrotate   => 'systemctl restart rsyslog',
+    require      => Class['profiles::logrotate'],
+  }
+}
