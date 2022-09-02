7caee4ef0214ee05abfb989cf8f11b5fdfe09b81
Jerald Sheets <jsheets@ssconsultinggroup.net>
2022-09-02 12:44:26 -0400

    typo in title of class

--- manifests/puppet_operational_dashboards.pp
+++ manifests/puppet_operational_dashboards.pp
@@ -1,6 +1,6 @@
 # This profile installs the necessary packages for the Puppet Operational Dashboards
 #
-class profiles::puppet_operationa_dashboards {
+class profiles::puppet_operational_dashboards {
 
   include puppet_operational_dashboards
 
