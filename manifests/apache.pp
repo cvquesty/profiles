1a96daa5d9b91ec244ecda7f1b45971c8cc8a456
Jerald Sheets <jsheets@ssconsultinggroup.net>
2022-09-02 11:57:26 -0400

    adding log rotation to Apache logs

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -5,4 +5,13 @@ class profiles::apache {
     default_vhost => false,
   }
 
+  logrtoate::rule { 'apache':
+    path          => '/var/log/apache2/*.log',
+    rotate        => 14,
+    mail          => 'root',
+    size          => '100m',
+    sharedscripts => true,
+    postrotate    => '/usr/bin/systemctl reload apache2',
+  }
+
 }
