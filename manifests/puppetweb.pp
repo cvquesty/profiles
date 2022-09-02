82c5440fbe53ef611ceffe3a325d1ec9a8da6536
Jerald Sheets <jsheets@ssconsultinggroup.net>
2022-09-02 12:11:46 -0400

    adding in puppetweb vhost for local server

---
+++ manifests/puppetweb.pp
@@ -0,0 +1,18 @@
+# Profile to configure Puppet Website for questy.org
+class profiles::puppetweb {
+
+  apache::vhosts { 'puppet.questy.org':
+    port    => 80,
+    docroot => '/var/www/html/puppetweb',
+    logroot => '/var/log/apache2/puppetweb',
+  }
+
+  logrotate::rule { 'puppetweb':
+    path          => '/var/log/apache2/puppetweb/*.log',
+    rotate        => '14',
+    mail          => 'questy@puppet.questy.org',
+    sharedscripts => true,
+    postrotate    => '/usr/bin/systemctl reload apache2',
+  }
+
+}
