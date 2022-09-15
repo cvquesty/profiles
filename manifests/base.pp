75b74a40a1f77e26f2b77ba4b368398ca1613aaf
Jerald Sheets <jsheets@ssconsultinggroup.net>
2022-09-15 09:41:50 -0400

    removing the puppetserver gem profile. Needs to call a different way

--- manifests/base.pp
+++ manifests/base.pp
@@ -4,7 +4,6 @@ class profiles::base {
   include profiles::ntp
   include profiles::logrotate
   include profiles::motd
-  include profiles::puppetserver_gem
   include profiles::rsyslog
   include profiles::ssh
 
--- manifests/puppetserver_gem.pp
+++
@@ -1,5 +0,0 @@
-# Profile to implement Puppetserver_Gem
-#
-class profiles::puppetserver_gem {
-  include puppetserver_gem
-}
